fa407eac8f4b225296887942105f28317a1d2e16
syket-das <saiketsd23@gmail.com>
2024-03-12 21:32:25 +0530

    sd

--- screens/profile/ngo/NgoProfile.jsx
+++ screens/profile/ngo/NgoProfile.jsx
@@ -38,7 +38,7 @@ const UserProfile = () => {
 
   useEffect(() => {
     fetchProfile();
-  }, []);
+  }, [profile]);
 
   return (
     <SafeAreaView
--- screens/profile/user/UserProfile.jsx
+++ screens/profile/user/UserProfile.jsx
@@ -39,7 +39,7 @@ const UserProfile = () => {
 
       setPoints(point);
     }
-  }, []);
+  }, [profile]);
 
   return (
     <SafeAreaView
