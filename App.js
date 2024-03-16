5cbb396be97b5baaf8104fff60ba1079d08ff953
syket-das <saiketsd23@gmail.com>
2024-03-16 08:26:19 +0530

    expo updates

--- App.js
+++ App.js
@@ -20,11 +20,28 @@ import SearchProfile from './screens/search/SearchProfile';
 import NgoPublicProfile from './screens/profile/ngo/NgoPublicProfile';
 import UserPublicProfile from './screens/profile/user/UserPublicProfile';
 import Providers from './components/Providers';
+import * as Updates from 'expo-updates';
 
 const Stack = createNativeStackNavigator();
+async function onFetchUpdateAsync() {
+  try {
+    const update = await Updates.checkForUpdateAsync();
 
+    if (update.isAvailable) {
+      await Updates.fetchUpdateAsync();
+      await Updates.reloadAsync();
+    }
+  } catch (error) {
+    // You can also add an alert() to see the error message in case of an error when fetching updates.
+    alert(`Error fetching latest Expo update: ${error}`);
+  }
+}
 export default function App() {
   const { authType, setAuthType } = useAuthStore((state) => state);
+
+  useEffect(() => {
+    onFetchUpdateAsync();
+  }, []);
 
   useEffect(() => {
     async function getAuthType() {
